a41cc5208ace16fb088a49bd3d59041935fc7bb3
sandee2847 <fugiganiyar14@gmail.com>
2023-04-22 15:40:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  function handleSearchQueryChange(event) {
+    setSearchQuery(event.target.value);
+  }
+
+  function handleSearchQuerySubmit(event) {
+    event.preventDefault();
+
+    fetch(`https://api.edamam.com/search?q=${searchQuery}&app_id=9ac29371&app_key=c672a2fca6e9a24e21e3e7fc1c134ebd`)
+      .then((response) => response.json())
+      .then((data) => setRecipes(data.hits))
+      .catch((error) => console.error(error));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        <h1>Recipe Finder</h1>
+        <form onSubmit={handleSearchQuerySubmit}>
+          <label>
+            Search for recipes:
+            <input type="text" value={searchQuery} onChange={handleSearchQueryChange} />
+          </label>
+          <button type="submit">Search</button>
+        </form>
+      </div>
+      {recipes.map((recipe) => (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', padding: 20 }} key={recipe.recipe.uri}>
+          <h2>{recipe.recipe.label}</h2>
+          <img src={recipe.recipe.image} alt={recipe.recipe.label} />
+          <p>{recipe.recipe.ingredients.map((ingredient) => ingredient.text).join(", ")}</p>
+          <a href={recipe.recipe.url}>View Recipe</a>
+        </div>
+      ))}
     </div>
   );
 }
